d82deb8e5655a18741d98decd1d4ad87f05cb864
ryan <ryan@duckduck.dev>
2020-07-01 11:09:30 -0400

    refactored WebClient to have retries on all calls

--- iot-api/Clients/WebClient.cs
+++ iot-api/Clients/WebClient.cs
@@ -11,61 +11,72 @@ namespace iot_api.Clients
         {
             Console.WriteLine($"[WebClient ({method})] uri: {uri}");
 
-            var webReq = (HttpWebRequest) WebRequest.Create(uri);
-
-            webReq.ContentType = "application/json";
-            webReq.Accept = "application/json";
-            webReq.Headers.Add("Content-Encoding", "utf-8");
-            webReq.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
-            webReq.KeepAlive = false;
-            webReq.Timeout = Configuration.Configuration.WebClientTimeout;
-            webReq.ServicePoint.ConnectionLeaseTimeout = 5000;
-            webReq.ServicePoint.MaxIdleTime = 5000;
-            webReq.ServicePoint.Expect100Continue = false;
-            webReq.ProtocolVersion = HttpVersion.Version11;
-
-            if (method == "GET") webReq.Method = WebRequestMethods.Http.Get;
-            if (method == "PUT") webReq.Method = WebRequestMethods.Http.Put;
-            if (method == "POST") webReq.Method = WebRequestMethods.Http.Post;
-            if (method == "DELETE") webReq.Method = method;
-
-            try
+            var retries = 0;
+            while (true)
             {
-                using (var webResp = webReq.GetResponse())
+                var webReq = (HttpWebRequest) WebRequest.Create(uri);
+
+                webReq.ContentType = "application/json";
+                webReq.Accept = "application/json";
+                webReq.Headers.Add("Content-Encoding", "utf-8");
+                webReq.CachePolicy = new RequestCachePolicy(RequestCacheLevel.NoCacheNoStore);
+                webReq.KeepAlive = false;
+                webReq.Timeout = Configuration.Configuration.WebClientTimeout;
+                webReq.ServicePoint.ConnectionLeaseTimeout = 5000;
+                webReq.ServicePoint.MaxIdleTime = 5000;
+                webReq.ServicePoint.Expect100Continue = false;
+                webReq.ProtocolVersion = HttpVersion.Version11;
+
+                if (method == "GET") webReq.Method = WebRequestMethods.Http.Get;
+                if (method == "PUT") webReq.Method = WebRequestMethods.Http.Put;
+                if (method == "POST") webReq.Method = WebRequestMethods.Http.Post;
+                if (method == "DELETE") webReq.Method = method;
+                try
                 {
-                    string webResponse;
-                    using (var dataStream = webResp.GetResponseStream())
+                    using (var webResp = webReq.GetResponse())
                     {
-                        if (dataStream == null)
-                            throw new WebException("Fail to retrieve response stream");
-
-                        using (var dataReader = new StreamReader(dataStream))
+                        string webResponse;
+                        using (var dataStream = webResp.GetResponseStream())
                         {
-                            webResponse = dataReader.ReadToEnd();
-                            dataReader.Close();
+                            if (dataStream == null)
+                                throw new WebException("Fail to retrieve response stream");
 
-                            webResp.Close();
-                            webResp.Dispose();
+                            using (var dataReader = new StreamReader(dataStream))
+                            {
+                                webResponse = dataReader.ReadToEnd();
+                                dataReader.Close();
+
+                                webResp.Close();
+                                webResp.Dispose();
+                            }
+
+                            dataStream.Flush();
                         }
 
-                        dataStream.Flush();
+                        webResp.Close();
+
+                        return string.IsNullOrEmpty(webResponse) ? null : webResponse;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    if (ex is WebException wex)
+                    {
+                        Console.WriteLine($"[WebClient ({method})] ERROR uri: {uri} error: {wex.Message}");
+                        Console.WriteLine(ex.StackTrace);
+
+                        if (wex.Status != WebExceptionStatus.Timeout ||
+                            retries >= Configuration.Configuration.DeviceRetries) throw;
+
+                        retries++;
+                        Console.WriteLine(
+                            $"[WebClient ({method})] RETRY {retries} of {Configuration.Configuration.DeviceRetries} for uri: {uri}");
+                        continue;
                     }
 
-                    webResp.Close();
-
-                    return string.IsNullOrEmpty(webResponse) ? null : webResponse;
-                }
-            }
-            catch (Exception ex)
-            {
-                if (ex is WebException wex)
-                {
-                    Console.WriteLine($"[WebClient ({method})] ERROR uri: {uri} error: {wex.Message}");
+                    Console.WriteLine($"[WebClient ({method})] ERROR uri: {uri} error: {ex.Message}");
                     throw;
                 }
-
-                Console.WriteLine($"[WebClient ({method})] ERROR uri: {uri} error: {ex.Message}");
-                throw;
             }
         }
 
